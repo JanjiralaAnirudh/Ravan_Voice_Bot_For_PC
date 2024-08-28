c9121fa895c224eac878f4ac8aa1b851fec952a7
JanjiralaAnirudh <anirudh66368@gmail.com>
2024-08-28 16:57:44 +0530

    functions.py

--- functions.py
+++ functions.py
@@ -23,14 +23,16 @@ def talk(text):
     engine.say(text)
     engine.runAndWait()
 
+#to tell the time
 def time():
       time = "curruct time is " + datetime.datetime.now().strftime("%H:%M %p")
       talk(time)
-
+#to tell the date
 def date():
       currentdate = datetime.date.today()
       talk(str(currentdate))
-
+    
+#to check the pc is connected to internet or not
 def check_internet():
     try:
         response = requests.get("http://www.google.com", timeout=3)
@@ -41,6 +43,7 @@ def check_internet():
         pass
     return net
 
+#to play songs on youtube
 def play_song(query):
       song = query.replace("play", "")
       try:
@@ -49,11 +52,13 @@ def play_song(query):
       except InternetException:
             pass
 
+#to ON / OFF bluetooth
 def allow_bluetooth():
         pyautogui.click(x=1697, y=1048)
         pyautogui.sleep(1)
         pyautogui.click(x=1660, y=440)
 
+"""
 def check_application(query):
     apps = []
 
@@ -97,8 +102,9 @@ def check_application(query):
         if i in query :
             talk("opening "+i)
             open_application(i)
-            break
+            break                   """
 
+#to check wheather the bluetooth is in ON state or OFF state
 def is_bluetooth_on():
     try:
         wmi_service = wmi.WMI()
@@ -107,6 +113,7 @@ def is_bluetooth_on():
     except Exception:
         return False
 
+#to check to which device the bluetooth is connected and tell the connected device name
 def bluetooth_access(query):
         if is_bluetooth_on() == True:     bluetooth = "on"
         else :  bluetooth = "off"
@@ -135,21 +142,27 @@ def bluetooth_access(query):
         if ("check" in query) or ("status" in query) or ("state" in query):
                 talk("your bluetooth is in "+bluetooth+" state")
 
+#to open pc settings
 def settings_access():
-        keyboard.press("windows+u")
+        keyboard.press("windows+i")
 
+#to increase the volume of the PC
 def increse_sound():
         pyautogui.press("volumeup",10)
 
+#to decrease the volume of the PC
 def decrese_sound():
         pyautogui.press("volumedown",10)
 
+#to mute volume of the PC
 def mute_sound():
         pyautogui.press("volumedown",100)
 
+#to unmute volume of the PC
 def unmute_sound():
         pyautogui.press("volumeup",35)
 
+#to open application in PC
 def open_application(application_name):
         keyboard.press_and_release("windows")
         pyautogui.sleep(1)
@@ -157,19 +170,22 @@ def open_application(application_name):
         pyautogui.sleep(1)
         keyboard.press_and_release("enter")
 
-
+#to lock the pc
 def lock_system():
         LockWorkStation = ctypes.windll.user32.LockWorkStation
         LockWorkStation()
 
+#to shut down the pc
 def shut_down():
     talk("bye bye !!!!!")
     os.system("taskkill /F /IM chrome.exe")
     os.system("shutdown /s /t 0")
 
+#to restart the PC
 def restart_sys():
         os.system("shutdown /r /t 1")
 
+#to check the status of the WiFi
 def wifi_status():
         wifi_on_or_off = ''
         connections = psutil.net_if_stats()
@@ -180,6 +196,7 @@ def wifi_status():
         
         talk("your wifi is in "+wifi_on_or_off)
 
+#to retrive name of the network WiFi connected
 def get_connected_wifi_network():
     result = subprocess.run(['netsh', 'wlan', 'show', 'interfaces'], capture_output=True, text=True)
     output = result.stdout.lower()
@@ -190,6 +207,7 @@ def get_connected_wifi_network():
         return output[ssid_start_index:ssid_end_index].strip()
     return None
 
+#here in this function is used to tell the connection name
 def wifi_net_con():
         connected_wifi = get_connected_wifi_network()
         if connected_wifi:
@@ -213,10 +231,12 @@ def get_battery_percentage():
     ctypes.windll.kernel32.GetSystemPowerStatus(ctypes.byref(status))
     return status.BatteryLifePercent
 
+#to get the battery percentage 
 def BatteryPercentage():
       percentage = get_battery_percentage()
       talk("your battery percentage is "+str(percentage))
 
+#to know whether the system is connected to charging or not
 def in_charge_or_not():
     battery = psutil.sensors_battery()
     if battery.power_plugged:
@@ -226,9 +246,11 @@ def in_charge_or_not():
           talk("your pc is not in charging &")
           BatteryPercentage()
 
+#to open the whatsapp
 def open_whatsapp():
       pywhatkit.open_web()
 
+#to check the internet speed
 def check_internet_speed():
     talk("just wait few seconds !!!")
     st = speedtest.Speedtest()
@@ -238,16 +260,19 @@ def check_internet_speed():
     talk(f"Download Speed: {download_speed:.2f} Mbps")
     talk(f"Upload Speed: {upload_speed:.2f} Mbps")
 
+#to know PC username 
 def get_username():
     username = getpass.getuser()
     talk("username is "+username.lower())
 
+#to save previous queries
 def save_query_to_csv(query):
     filename = 'aa_bot_queries.csv'
     with open(filename, 'a', newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow([query])
 
+#to retrive previous queries
 def get_queries():
     filename = 'aa_bot_queries.csv'
     # Open the file and read the queries from CSV
@@ -261,15 +286,18 @@ def get_queries():
     talk("your last query is ")
     talk(previous_query)
 
+#to open the camera
 def open_camera():
     subprocess.run('start microsoft.windows.camera:', shell=True)
-    
+
+#to set timer
 def countdown_timer(minutes):
     seconds = minutes * 60
     talk("timer as been started")
     time.sleep(seconds)
     talk("Time's up!")
 
+#used for closing the tabs
 def tab_closing():
     talk("closing !!!")
     active_window = gw.getActiveWindow()
